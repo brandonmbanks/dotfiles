7ed44cd834656181af9a3c3f37c334944ae2bb57
Brandon Banks <10030196+brandonmbanks@users.noreply.github.com>
2024-02-16 15:43:56 -0500

    add pane navigation; add highlight to rosepine

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,11 +3,9 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 -- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = "rose-pine-moon"
 
 -- font
 config.font = wezterm.font({
@@ -20,8 +18,36 @@ config.keys = {
   {
     key = "w",
     mods = "CTRL|ALT",
-    action = wezterm.action.CloseCurrentPane({ confirm = true }),
+    action = act.CloseCurrentPane({ confirm = true }),
   },
+  {
+    key = "LeftArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Left"),
+  },
+  {
+    key = "RightArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Right"),
+  },
+  {
+    key = "UpArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Up"),
+  },
+  {
+    key = "DownArrow",
+    mods = "CTRL|SHIFT",
+    action = act.ActivatePaneDirection("Down"),
+  },
+}
+
+local scheme = wezterm.get_builtin_color_schemes()["rose-pine-moon"]
+scheme.selection_bg = "#5e5b73"
+
+config.color_scheme = "rose-pine-moon-hl"
+config.color_schemes = {
+  ["rose-pine-moon-hl"] = scheme,
 }
 
 -- and finally, return the configuration to wezterm
